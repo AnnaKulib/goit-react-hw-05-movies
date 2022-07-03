5ca1062dcfe677b670d2f6110fc1fcbc3a949b8a
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-07-03 17:07:46 +0300

    adds all files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
+import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Toaster } from "react-hot-toast" 
+import Loader from "./Loader";
+import Container from "./Container";
+
+// import "./App.css"
+
+const Navigation = lazy(() => import("./Navigation" ));
+const HomePage = lazy(() => import("./HomePage"));
+const MovieDetailsPage = lazy(() => import("./MovieDetailsPage" ));
+const MoviesPage = lazy(() => import("./MoviesPage" ));
+const Cast = lazy(() => import("./Cast" ));
+const Reviews = lazy(() => import("./Reviews"));
+
+// import Navigation from "./Navigation";
+// import HomePage from "./HomePage";
+// import MoviesPage from "./MoviesPage"
+// import MovieDetailsPage  from "./MovieDetailsPage";
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Container>
+    <Suspense fallback={<Loader/>}>
+      <Navigation />
+      <Routes>
+            <Route path="/" element={<HomePage/>} />
+          <Route path="/movies" element={<MoviesPage/>} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />}/>
+            <Route path="reviews" element={<Reviews/>}/>
+          </Route>
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Toaster position="top-right" />
+      </Suspense>
+      </Container>
+    </>
+      
   );
-};
+}
+
+export default App;
